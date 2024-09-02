3cd04969040ae7cd3cd3de7658318bb394d6b0d6
Ankit Raj <w.ankitrajdev@gmail.com>
2024-09-02 12:16:15 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+      origin: ["https://login-xjgc.onrender.com"],
+      methods: ["GET", "POST"],
+  }
+));
 app.use(bodyParser.json());
 
 const uri = process.env.MONGO_URI;
